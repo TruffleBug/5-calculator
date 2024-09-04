4601fd70997510f76d070e12390e2fda6feae3fe
TruffleBug <lisa.ho1022@gmail.com>
2024-09-04 13:48:24 -0600

    Add basic calculator functions into js
    Added basic calculator functions into script.js. Functions are
    functional through command line, but user input functionality not yet
    set up.

---
+++ script.js
@@ -0,0 +1,20 @@
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => num1 / num2;
+
+let num1
+
+let num2
+
+let operator
+
+function operate(num1, num2, operator) {
+    if (operator === 'add') add(num1, num2);
+    else if (operator === 'subtract') subtract(num1, num2);
+    else if (operator === 'multiply') multiply(num1, num2);
+    else divide(num1, num2);
+}
