569ef53a9d3b4ee94530891343f4e6ff3f41cd7d
TruffleBug <lisa.ho1022@gmail.com>
2024-09-04 14:40:36 -0600

    Click on numbers to display at top
    Clicking on numbers will now show up in the display. Still no actual
    calculator functionality.

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
 
-let num1
+let num1;
 
-let num2
+let num2;
 
-let operator
+let operator;
 
 function operate(num1, num2, operator) {
     if (operator === 'add') add(num1, num2);
     else if (operator === 'subtract') subtract(num1, num2);
     else if (operator === 'multiply') multiply(num1, num2);
     else divide(num1, num2);
-}
+};
+
+let display = document.querySelector('.display');
+let numbers = document.querySelectorAll('.buttons .numbers button');
+
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        let displayValue = number.textContent
+        display.textContent = displayValue
+    })
+})
