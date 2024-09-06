f71ede6057722cd9c97f899dfa9c94e5ebbed169
TruffleBug <lisa.ho1022@gmail.com>
2024-09-06 10:46:02 -0600

    Calculator chains input functions
    You can now input multiple functions without hitting the = button.
    You can also continue functions after hitting the = button, but you
    cannot start a brand new function after hitting =. I think the issue
    can be solved by setting the operator equal to a blank string after
    hitting =, then conditionally analyzing if the operator is a blank
    string within the click event on the numbers.

--- script.js
+++ script.js
@@ -6,13 +6,12 @@ const multiply = (num1, num2) => result = num1 * num2;
 
 const divide = (num1, num2) => result = num1 / num2;
 
-let num1;
-let num2;
+let num1 = 0;
+let num2  = 0;
 let displayValue = '';
 let operator = '';
 
 function operate(num1, num2, operator) {
-    // console.log({operator})
     if (operator === 'add') add(num1, num2);
     else if (operator === 'subtract') subtract(num1, num2);
     else if (operator === 'multiply') multiply(num1, num2);
@@ -27,6 +26,18 @@ let clear = document.querySelector('#clear');
 
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
+        // if(operator === '') {
+        //     displayValue = '';
+        //     displayValue = displayValue.concat(number.textContent);
+        //     display.textContent = displayValue;
+        //     console.log({displayValue});     
+        //             console.log('operator is undefined')       
+        // } else {
+        //     displayValue = displayValue.concat(number.textContent);
+        //     display.textContent = displayValue;
+        //     console.log({displayValue});
+        //             console.log('has operator')
+        // }
         displayValue = displayValue.concat(number.textContent);
         display.textContent = displayValue;
         console.log({displayValue});
@@ -36,11 +47,20 @@ numbers.forEach((number) => {
 
 operators.forEach((operatorInput) => {
     operatorInput.addEventListener('click', () => {
-        num1 = displayValue;
-                // console.log({num1});
-        operator = operatorInput.id;
-                console.log({operator});
-        displayValue = '';
+        if(num1 === 0) {
+            num1 = displayValue;
+            operator = operatorInput.id;
+                    console.log({operator});
+            displayValue = '';
+        } else {
+            num2 = displayValue;
+            operate(num1, num2, operator);
+                    console.log(result)
+            num1 = result;
+            display.textContent = result;
+            operator = operatorInput.id;
+            displayValue = '';
+        }
     })
 })
 
@@ -50,6 +70,7 @@ equals.addEventListener('click', () => {
     operate(num1, num2, operator);
                 console.log(`${num1} ${operator} ${num2} = ${result}`)
     display.textContent = result;
+    // operator = '';
 })
 
 clear.addEventListener('click', () => {
@@ -58,5 +79,5 @@ clear.addEventListener('click', () => {
     operator = '';
     displayValue = '';
     display.textContent = 0;
-    console.log(`${num1} ${operator} ${num2} = ${result}`)
+                console.log(`${num1} ${operator} ${num2} = ${result}`)
 })
